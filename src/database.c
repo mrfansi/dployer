6ee31947f3368fbb056d97ba364996d2cc3c49cc
Muhammad Irfan <mrfansi@outlook.com>
2024-08-14 11:29:00 +0700

    Update

--- src/database.c
+++ src/database.c
@@ -61,47 +61,8 @@ void open_database(const char *db_name)
         log_message(SUCCESS, SUCCESS_SYMBOL, "Opened database successfully.");
     }
 
-    // Check if the repositories table exists and initialize the database if needed
-    const char *check_table_sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='repositories';";
-    sqlite3_stmt *stmt;
-    if (sqlite3_prepare_v2(db, check_table_sql, -1, &stmt, 0) != SQLITE_OK)
-    {
-        log_message(ERROR, ERROR_SYMBOL, "Failed to check for repositories table.");
-        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
-        sqlite3_finalize(stmt);
-        sqlite3_close(db);
-        exit(1);
-    }
-
-    int table_exists = 0;
-    if (sqlite3_step(stmt) == SQLITE_ROW)
-    {
-        table_exists = 1;
-    }
-    sqlite3_finalize(stmt);
-
-    // If the repositories table doesn't exist, initialize the database
-    if (!table_exists)
-    {
-        const char *sql = "CREATE TABLE IF NOT EXISTS repositories ("
-                          "id TEXT PRIMARY KEY,"
-                          "git_url TEXT NOT NULL,"
-                          "destination_folder TEXT NOT NULL,"
-                          "branch_name TEXT NOT NULL,"
-                          "docker_image_tag TEXT NOT NULL,"
-                          "docker_port TEXT NOT NULL,"
-                          "last_updated DATETIME DEFAULT CURRENT_TIMESTAMP"
-                          ");";
-        char *err_msg = NULL;
-        if (sqlite3_exec(db, sql, 0, 0, &err_msg) != SQLITE_OK)
-        {
-            log_message(ERROR, ERROR_SYMBOL, "SQL error occurred.");
-            fprintf(stderr, "SQL error: %s\n", err_msg);
-            sqlite3_free(err_msg);
-            exit(1);
-        }
-        log_message(SUCCESS, SUCCESS_SYMBOL, "Database initialized successfully.");
-    }
+    // Initialize the database
+    initialize_database();
 }
 
 void initialize_database()
@@ -112,12 +73,11 @@ void initialize_database()
                       "destination_folder TEXT NOT NULL,"
                       "branch_name TEXT NOT NULL,"
                       "docker_image_tag TEXT NOT NULL,"
-                      "docker_port TEXT NOT NULL," // Add this line
+                      "docker_port TEXT NOT NULL,"
                       "last_updated DATETIME DEFAULT CURRENT_TIMESTAMP"
                       ");";
 
     execute_query(sql);
-    log_message(SUCCESS, SUCCESS_SYMBOL, "Database initialized successfully.");
 }
 
 void close_database()
